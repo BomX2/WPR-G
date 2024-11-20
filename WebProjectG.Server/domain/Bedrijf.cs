5adb688e0284ed6cc05f5e7c9f2c17256a5c6a44
Camilonline <118902474+Camilonline@users.noreply.github.com>
2024-11-20 22:12:39 +0100

    Changes to classes, added abonnemnt

--- WebProjectG.Server/domain/Bedrijf.cs
+++ WebProjectG.Server/domain/Bedrijf.cs
@@ -1,17 +1,23 @@
 ﻿namespace WebProjectG.Server.domain
 {
-    public abstract class Bedrijf
+    public class Bedrijf : Klant
     {
-
       public string BedrijfsNaam { get; set; }
       public int Kvknummer { get; set; }
        public string Domeinnaam { get; set; }
-        public List<ZakelijkeHuurder> zakelijkeHuurders = [];
-        public Bedrijf(string bedrijfsNaam, int kvknummer, string domeinnaam) 
+        public List<ZakelijkeHuurder> ZakelijkeHuurders { get; private set; }
+        public List<WagenParkBeheerder> WagenParkBeheerders { get; private set; }
+
+        public Bedrijf(string adres, string telefoonnummer, string email,
+                       string bedrijfsNaam, int kvknummer, string domeinnaam)
+            : base(adres, telefoonnummer, email) 
         { 
             BedrijfsNaam = bedrijfsNaam;
             Kvknummer = kvknummer;
             Domeinnaam = domeinnaam;
+            ZakelijkeHuurders = new List<ZakelijkeHuurder>();
+            WagenParkBeheerders = new List<WagenParkBeheerder>();
+        }
         }
 
     }
