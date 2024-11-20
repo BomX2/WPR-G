bbf436397e678f9829dd54d402e0697e7b897dd5
Camilonline <118902474+Camilonline@users.noreply.github.com>
2024-11-20 17:30:32 +0100

    Voertuig

--- WebProjectG.Server/domain/Voertuig.cs
+++ WebProjectG.Server/domain/Voertuig.cs
@@ -1,6 +1,23 @@
 ﻿namespace WebProjectG.Server.domain
 {
-    public class Voertuig
+    abstract public class Voertuig
     {
+        public string HuurStatus { get; set; }
+        public string Merk { get; set; }
+        public string Type { get; set; }
+        public string Kenteken { get; set; }
+        public string Kleur { get; set; }
+        public int AanschafJaar { get; set; }
+
+        protected Voertuig(string huurStatus, string merk, string type, string kenteken, string kleur, int aanschafJaar)
+        {
+            HuurStatus = huurStatus;
+            Merk = merk;
+            Type = type;
+            Kenteken = kenteken;
+            Kleur = kleur;
+            AanschafJaar = aanschafJaar;
+        }
+
     }
 }
