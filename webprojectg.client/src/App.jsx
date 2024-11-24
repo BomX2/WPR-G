6d0f0f909384413c74d56bd46d749cf7cc14e7fa
Bartho <23091029@student.hhs.nl>
2024-11-24 20:28:48 +0100

    testing frontend Met usestate

--- webprojectg.client/src/App.jsx
+++ webprojectg.client/src/App.jsx
@@ -1,30 +1,35 @@
-import logo from './assets/logo.png';
-import ReactDOM from 'react-dom/client';
-import './App.css';
+    import logo from './assets/logo.png';
+    import ReactDOM from 'react-dom/client';
+    import './App.css';
+    import { useState } from 'react';
 
 function App() {
-
-    const frontpage = (
+    const [code, setCode] = useState("");
+    const secretLogIn = (waarde) => {
+        setCode(waarde);
+        if (waarde === "mib") {
+            console.log("Geheime code is ingevoerd");
+        }
+    };
+    return (
         <>
             <img
-                src={logo} 
+                src={logo}
                 alt="Logo van CarAndAll"
                 style={{ width: "200px", height: "auto" }}
 
             />
             <h2>CarAndAll</h2>
             <p> CarAndAll is een bedrijf dat specialiseert in het verhuren van verschillende soorten voertuigen</p>
-            <a
-                href="https://www.google.nl/maps/place/De+Haagse+Hogeschool/@52.0670747,4.3213991,17z/data=!3m1!4b1!4m6!3m5!1s0x47c5b6e175fe3619:0x9d1994a880751d7a!8m2!3d52.0670747!4d4.323974!16s%2Fm%2F027_cbq?entry=ttu&g_ep=EgoyMDI0MTExOC4wIKXMDSoASAFQAw%3D%3D"
-                target="_blank"
-                rel ="noopener noreferrer"
+            <p>{code }</p>
+            <input onInput={(e) => secretLogIn(e.target.value)}
+                type="text"
             >
-               De haagse hogeschool locatie Den Haag op google maps
-            </a>
+            </input>
         </>
     );
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(frontpage);
-    }
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<App />);
 
-export default App;
+    export default App;
