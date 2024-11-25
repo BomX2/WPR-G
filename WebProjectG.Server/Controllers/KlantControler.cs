17805766495d029085f36e143eee1ad4dca56dd9
Joshua <23147253@student.hhs.nl>
2024-11-25 14:03:16 +0100

    KlanControler.cs klas gemaakt boohoo testing nog wip met Swagger.

---
+++ WebProjectG.Server/Controllers/KlantControler.cs
@@ -0,0 +1,8 @@
+﻿namespace WebProjectG.Server.Controllers
+{
+    public class KlantControler
+    {
+        private readonly AppDbContext _context = new ;
+        private readonly IEmailService _emailService;
+    }
+}
