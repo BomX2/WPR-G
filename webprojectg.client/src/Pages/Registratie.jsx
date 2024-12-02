cde02603c26390a36545cbbecccaa4029066c5d8
onnok <onnokok5@gmail.com>
2024-12-02 13:21:39 +0100

    css registratie

--- webprojectg.client/src/Pages/Registratie.jsx
+++ webprojectg.client/src/Pages/Registratie.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import "./Registratie.css"
 
 const Registratie = () => {
 
 
     return (
         <>
-            <h1>CarAndAll</h1>
-            <div>
-                
+            <div className='container'>
+                <div className="header">
+                    <div className="text">Sign Up</div>
+
+                </div>
+                <div className="inputs">
+                    <div className="input">
+                    <input type="naam"/>
+                    </div>
+                    <div className="input">
+                        <input type="adress" />
+                    </div>
+                    <div className="input">
+                        <input type="telefoon nummer" />
+                    </div>
+                    <div className="input">
+                        <input type="email" />
+                    </div>
+                    <div className="input">
+                        <input type="wachtwoord" />
+                    </div>
+                </div>
+                </div>
             </div>
         </>
     );
