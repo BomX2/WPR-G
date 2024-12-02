a9546b5b85a1988606291d7ac7af8c7b19adedd0
Joshua <23147253@student.hhs.nl>
2024-12-02 11:36:50 +0100

    -- All endpoints made. Currently Error on the ID en id, working on a solution.

--- WebProjectG.Server/Controllers/ValuesController.cs
+++ WebProjectG.Server/Controllers/ValuesController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using SQLitePCL;
+using WebProjectG.Server.domain;
 
 namespace WebProjectG.Server.Controllers
 {
@@ -8,13 +11,57 @@ namespace WebProjectG.Server.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        [HttpPost]
-        public async Task<ActionResult<()>>PostTest()
-        {
-            //_context.Test.Add();
-            //await _context.SaveChangesAsync()
+        private HuurContext _huurContext = new HuurContext();
 
-            return CreatedAtAction();
+        [HttpPost]
+        public async Task<ActionResult<Klant>> PostTest(Klant klant)
+        {
+            _huurContext.klanten.Add(klant);
+            await _huurContext.SaveChangesAsync();
+
+            return CreatedAtAction("GetKlant", new { id = klant.Id}, klant);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Klant>> GetKlant(int id)
+        {
+            var klant = await _huurContext.klanten.FindAsync(id); 
+
+            if (klant == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(klant);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutKlant(int id, Klant klant)
+        {
+            if (id != klant.Id) { return BadRequest(); }
+
+            _huurContext.Entry(klant).State = EntityState.Modified;
+
+            try { await _huurContext.SaveChangesAsync(); }
+
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_huurContext.klanten.Any(e => Id == id)) { return NotFound(); }
+                else { throw; }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteKlant(int id)
+        {
+            var klant = await _huurContext.klanten.FindAsync(id);
+            if (klant == null) { return NotFound(); }
+
+            _huurContext.klanten.Remove(klant);
+            await _huurContext.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
