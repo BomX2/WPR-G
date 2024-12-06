261f14c50aa331ccd1bcbcf9c795f3dcf91dcffb
BomX2 <onnokok5@gmail.com>
2024-12-07 00:00:15 +0100

    ......

--- webprojectg.client/src/App.jsx
+++ webprojectg.client/src/App.jsx
@@ -3,10 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './componements/Navigation';
 import Home from './Pages/Home';
 import About from './pages/About';
-import Registratie from './Pages/Registratie'
-import Inlog from './Pages/Inlog'
-import Catalogus from './Pages/Catalogus'
-import AccSettings from './pages/AccountSettings'
+import Registratie from './Pages/Registratie';
+import Inlog from './Pages/Inlog';
+import Catalogus from './Pages/Catalogus';
+import AccSettings from './Pages/AccountSettings';
+import Product from './Pages/Product';
 
 const App = () => {
      
@@ -17,7 +18,8 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />\
-                <Route path="/catalogus/:id" element={<Catalogus/>}/>
+                <Route path="/catalogus" element={<Catalogus />} />
+                <Route path="/Product/:id" element={<Product />} />
                 <Route path="/inlog" element={<Inlog/> } />
                 <Route path="/registratie" element={<Registratie />} />
                 <Route path="/AccountSettings" element={<AccSettings /> } />
--- webprojectg.client/src/Pages/Catalogus.jsx
+++ webprojectg.client/src/Pages/Catalogus.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { useParams } from "react-router-dom";
 
 export default function Catalogus() {
-    const { id } = useParams()
     return (
         <div>
-            <h1>catalogus nr {id}</h1>
+            <h1>catalogus nr </h1>
              
         </div>
     );
