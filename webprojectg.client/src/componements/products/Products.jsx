283bace263759168db9ceddd74d1d691c1777bab
BomX2 <onnokok5@gmail.com>
2024-12-10 22:08:50 +0100

    catalogus pagina

--- webprojectg.client/src/componements/products/Products.jsx
+++ webprojectg.client/src/componements/products/Products.jsx
@@ -1,11 +1,18 @@
-import "./Products.ccs";
-
-export default function Products() {
+import "./Products.css";
+import carImage from "../../image/kever.jpg";
+import { Link } from "react-router-dom";
+export default function Products({ props }) {
 
     return (
-        <section>
-            <section>
-                <img src="https://www.google.com/imgres?q=auto&imgurl=https%3A%2F%2Fwww.duurzaamnieuws.nl%2Fwp-content%2Fuploads%2F2021%2F10%2Fstella-vita-zonne-auto.jpg&imgrefurl=https%3A%2F%2Fwww.duurzaamnieuws.nl%2Fhoe-slimme-autos-een-gevaar-vormen-voor-jouw-cyberveiligheid%2F&docid=c5dK-x_tCvEtcM&tbnid=ujJwpnzW9-RApM&vet=12ahUKEwjckdnV8pqKAxWLT0EAHX_HHyYQM3oECGkQAA..i&w=800&h=400&hcb=2&ved=2ahUKEwjckdnV8pqKAxWLT0EAHX_HHyYQM3oECGkQAA" />
+        <section className="product-container">
+            <section className="product">
+                <div className="product-content">
+                    <img src={carImage} alt="auto" className="product-image" />
+                    <div className="product-details">
+                    <h3>auto</h3>
+                        <Link to={`/Product/${props}`}>Klik hier</Link>
+                    </div>
+                </div>
             </section>
         </section>
     );
