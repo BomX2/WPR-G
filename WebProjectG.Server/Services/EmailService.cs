c256c462efbec2c91411e108e0e29ce36c87e134
Joshua <23147253@student.hhs.nl>
2024-12-11 14:06:49 +0100

    Email Service - work in progress.

---
+++ WebProjectG.Server/Services/EmailService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.Win32;
+using System.Net.Mail;
+using System;
+using MimeKit;
+
+namespace WebProjectG.Server.Services
+{
+    public class EmailService
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public EmailService(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("No Reply", _emailSettings.FromAddress));
+            emailMessage.To.Add(new MailboxAddress("", toEmail));
+            emailMessage.Subject = subject;
+
+            var bodyBuilder = new BodyBuilder
+            {
+                HtmlBody = body
+            };
+
+            emailMessage.Body = bodyBuilder.ToMessageBody();
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(_emailSettings.SmtpServer, _emailSettings.SmtpPort, false);
+                await client.AuthenticateAsync(_emailSettings.SmtpUser, _emailSettings.SmtpPassword);
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
