ddf1750a077f7fe570c6f54eaa590287841aa0c1
Camilonline <118902474+Camilonline@users.noreply.github.com>
2024-12-11 11:25:32 +0100

    movedDbContext to controller folder

---
+++ WebProjectG.Server/domain/Gebruiker/Controllers/GebruikerDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebProjectG.Server.domain.Gebruiker.Controllers
+{
+    public class GebruikerDbContext : IdentityDbContext<Gebruiker>
+    {
+        public DbSet<Bedrijf> Bedrijven { get; set; } // Include Bedrijf table in the schema
+
+        public GebruikerDbContext(DbContextOptions<GebruikerDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Gebruiker>(entity =>
+            {
+                entity.Property(e => e.Adres)
+                    .IsRequired()
+                    .HasMaxLength(256); // Adjust length as necessary
+
+                entity.Property(e => e.PhoneNumber)
+                    .IsRequired();
+            });
+        }
+    }
+}
