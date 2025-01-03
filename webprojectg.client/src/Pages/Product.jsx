e44f6d1f2849e01203d8ea6c0ec97d66eb33d076
Bartho <23091029@student.hhs.nl>
2025-01-03 11:44:34 +0100

    nieuwe branch huuraanvraag

--- webprojectg.client/src/Pages/Product.jsx
+++ webprojectg.client/src/Pages/Product.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState } from 'react';
 import { useParams } from "react-router-dom";
+
 export default function Product() {
+    const [modalWindow, setModalWindow] = useState("");
     const { id } = useParams()
+    const OnButtonClick = () => {
+        setModalWindow(true);
+    }
+    const  CloseWindow = () => {
+        setModalWindow(false);
+    }
     return (
         <div>
             <h1>catalogus nr {id}</h1>
-
+            <button onClick={OnButtonClick} >klik hier om een huuraanvraag te maken voor deze auto</button>
+            {modalWindow && (
+                <div>
+                        <h2>Huuraanvraag</h2>
+                        <p>Vul de gegevens in voor de huuraanvraag van deze auto.</p>
+                        {/* Hier kun je een formulier plaatsen */}
+                    <button onClick={CloseWindow}>Sluiten</button>
+             </div>
+            )}
         </div>
     );
-};
+}
