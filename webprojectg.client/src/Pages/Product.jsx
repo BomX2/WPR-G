ea52bd2e04818e9e8b456990ff62fb78ec24d484
Bartho <23091029@student.hhs.nl>
2025-01-03 15:29:44 +0100

    k

--- webprojectg.client/src/Pages/Product.jsx
+++ webprojectg.client/src/Pages/Product.jsx
@@ -1,8 +1,14 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import { useParams } from "react-router-dom";
-
+import './modal.css';
 export default function Product() {
     const [modalWindow, setModalWindow] = useState("");
+    //const [datum, setDatum] = useState("");
+    const [naam, setNaam] = useState("");
+    const [email, setEmail] = useState("");
+    const [telnummer, setTelNummer] = useState("")
     const { id } = useParams()
     const OnButtonClick = () => {
         setModalWindow(true);
@@ -13,14 +19,30 @@ export default function Product() {
     return (
         <div>
             <h1>catalogus nr {id}</h1>
+            <DatePicker
+                selected={new Date()}
+                onChange={(date) => console.log(date)}
+            />
             <button onClick={OnButtonClick} >klik hier om een huuraanvraag te maken voor deze auto</button>
             {modalWindow && (
-                <div>
+                <div className="modal-overlay">
+                <div className="modal-content" >
                         <h2>Huuraanvraag</h2>
                         <p>Vul de gegevens in voor de huuraanvraag van deze auto.</p>
-                        {/* Hier kun je een formulier plaatsen */}
+                        <form>
+                            <input type="text" value={naam} placeholder="Voer hier uw voornaam en achternaam in." onChange={(e) => setNaam(e.target.value)}>
+                            </input>
+                            <input type="email" value={email} placeholder="Voer hier uw emailadres in." onChange={(e) => setEmail(e.target.value)}>
+                            </input>
+                            <input type="tel" value={telnummer} placeholder="Voer hier uw telefoonnummer in." onChange={(e) => setTelNummer(e.target.value)}>
+                            </input>
+                           
+                        </form>
+                   
+                    <button type="submit">verstuur huuraanvraag</button>
                     <button onClick={CloseWindow}>Sluiten</button>
-             </div>
+                    </div>
+                </div>
             )}
         </div>
     );
