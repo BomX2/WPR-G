644f18a2453a0d7e842c240f457eaccde9dc3e18
onnok <onnokok5@gmail.com>
2025-01-05 14:43:10 +0100

    catalogus pagina werkend

--- webprojectg.client/src/componements/products/Products.jsx
+++ webprojectg.client/src/componements/products/Products.jsx
@@ -6,11 +6,9 @@ export default function Products({ auto }) {
     return (
         <section className="product-container">
             <section className="product">
+                <h3 className="product-title">{auto.merk} {auto.type}</h3>
                 <div className="product-content">
-                    <h3>{auto.merk} {auto.type}</h3>
-                   <div>
-                        <img src={carImage} alt="auto" className="product-image" />
-                    </div>
+                    <img src={carImage} alt="auto" className="product-image" />
                     <div className="product-details">
                         <h3>prijs per dag :{auto.prijsPerDag} euro</h3>
                         <h3>{ auto.huurStatus }</h3>
