ad77635ec042c7e882fe9adfbf3584639e1e578e
Bartho <23091029@student.hhs.nl>
2025-01-19 15:30:26 +0100

    a

--- WebProjectG.Server/Migrations/Huur/20250119142934_initialhuuren.Designer.cs
+++ WebProjectG.Server/Migrations/Huur/20250119142934_initialhuuren.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebProjectG.Server.domain;
 
@@ -11,9 +12,11 @@ using WebProjectG.Server.domain;
 namespace WebProjectG.Server.Migrations.Huur
 {
     [DbContext(typeof(HuurContext))]
-    partial class HuurContextModelSnapshot : ModelSnapshot
+    [Migration("20250119142934_initialhuuren")]
+    partial class initialhuuren
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
