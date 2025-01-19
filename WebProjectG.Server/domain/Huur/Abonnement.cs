7c7bc633a7f36672ca09589a3a916a46c97e364c
Bartho <23091029@student.hhs.nl>
2025-01-19 13:37:36 +0100

    commit

--- WebProjectG.Server/domain/Huur/Abonnement.cs
+++ WebProjectG.Server/domain/Huur/Abonnement.cs
@@ -8,7 +8,6 @@ namespace WebProjectG.Server.domain.Huur
     public class Abonnement
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int AbonnementID { get; set; }
         public string AbonnementType { get; set; }
         public decimal Prijs { get; set; }
