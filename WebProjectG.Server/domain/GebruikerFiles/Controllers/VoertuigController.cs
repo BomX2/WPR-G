9006764c3b6d418a208f8a7a1fb7c97c8b661ca9
onnok <onnokok5@gmail.com>
2025-01-23 02:22:51 +0100

    hard filter voertuigen

--- WebProjectG.Server/domain/GebruikerFiles/Controllers/VoertuigController.cs
+++ WebProjectG.Server/domain/GebruikerFiles/Controllers/VoertuigController.cs
@@ -22,19 +22,36 @@ namespace WebProjectG.Server.domain.GebruikerFiles.Controllers
         {
             var query = _huurContext.Voertuigen.AsQueryable();
 
-            if (queryParams.StartDatum != null && queryParams.EindDatum != null)
+            if (queryParams.StartDatum != null && queryParams.EindDatum != null &&
+        !string.IsNullOrEmpty(queryParams.OphaalTijd) && !string.IsNullOrEmpty(queryParams.InleverTijd))
             {
                 var startDatum = queryParams.StartDatum.Value;
                 var eindDatum = queryParams.EindDatum.Value;
 
+                // Zet ophaaltijd en inlevertijd om naar numerieke waarden voor vergelijking (bijvoorbeeld ochtend = 0, middag = 1, avond = 2)
+                var dagdelen = new Dictionary<string, int> { { "ochtend", 0 }, { "middag", 1 }, { "avond", 2 } };
+
+                int OphaalTijd = dagdelen[queryParams.OphaalTijd.ToLower()];
+                int InleverTijd = dagdelen[queryParams.InleverTijd.ToLower()];
+
                 var bezetteAuto = await _huurContext.Aanvragen
-                    .Where(a => a.StartDatum <= eindDatum &&
-                                a.EindDatum >= startDatum)
+                    .Where(a =>
+                        // Controleer overlappende verhuurperiodes
+                        (a.StartDatum < eindDatum || (a.StartDatum == eindDatum && dagdelen[a.ophaaltijd] <= InleverTijd)) &&
+                        (a.EindDatum > startDatum || (a.EindDatum == startDatum && dagdelen[a.inlevertijd] >= OphaalTijd)) &&
+                        // Zorg dat er minimaal 1 dagdeel ruimte zit
+                        (a.EindDatum < startDatum || (a.EindDatum == startDatum && dagdelen[a.inlevertijd] < OphaalTijd - 1)))
                     .Select(a => a.Kenteken)
                     .Distinct()
                     .ToListAsync();
 
+                // Filter voertuigen die niet beschikbaar zijn
                 query = query.Where(v => !bezetteAuto.Contains(v.Kenteken));
+            }
+
+            if (!string.IsNullOrEmpty(queryParams.Soort))
+            {
+                query = query.Where(v => v.soort.ToLower() == queryParams.Soort.ToLower());
             }
 
             var voertuigen = await query.ToListAsync();
